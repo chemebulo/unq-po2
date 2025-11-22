4a6a12a5e7f08347d333df97f856c727d3859c28
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-11-22 18:05:10 -0300

    Ejercicio 1.1 TP11 Terminado

--- test/ar/edu/unq/po2/tp11/sistema_de_publicaciones/CriterioTituloTest.java
+++ test/ar/edu/unq/po2/tp11/sistema_de_publicaciones/CriterioTituloTest.java
@@ -2,13 +2,38 @@ package ar.edu.unq.po2.tp11.sistema_de_publicaciones;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CriterioTituloTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private CriterioTitulo criterio;
+	private ArticuloAcademico articulo;
+	private Autor autor;
+	private List<String> palabrasClave;
+	private List<Autor> autores;
+	
+	@BeforeEach
+	void setUp() {
+		this.criterio = new CriterioTitulo("IA Avanzada");
+		
+		this.autor = new Autor("Carlos Rodriguez", "Universidad Nacional de Quilmes");
+		this.autores = new ArrayList<Autor>();
+		autores.add(autor);
+		
+		this.palabrasClave = new ArrayList<String>();
+		String palabra1 = "IA";
+		String palabra2 = "Tecnologia";
+		palabrasClave.add(palabra1);
+		palabrasClave.add(palabra2);
+		
+		this.articulo = new ArticuloAcademico("IA Avanzada", autores, "Investigacion", "UNQ", palabrasClave);
 	}
-
+	
+	@Test
+	public void funcionamientoGeneral() {
+		assertTrue(criterio.cumple(articulo));
+	}
 }
--- test/ar/edu/unq/po2/tp11/sistema_de_publicaciones/SistemaDePublicacionesTest.java
+++ test/ar/edu/unq/po2/tp11/sistema_de_publicaciones/SistemaDePublicacionesTest.java
@@ -2,12 +2,63 @@ package ar.edu.unq.po2.tp11.sistema_de_publicaciones;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SistemaDePublicacionesTest {
-
+	private SistemaDePublicaciones sistema;
+	private Investigador investigador;
+	private Conector conector;
+	private ArticuloAcademico articulo;
+	
+	@BeforeEach
+	public void setUp() {
+		List<Autor> autores = new ArrayList<Autor>();
+		Autor autor = new Autor("Carlos Rodriguez", "Universidad Nacional de Quilmes");
+		autores.add(autor);
+		
+		List<String> palabrasClave = new ArrayList<String>();
+		String palabra1 = "IA";
+		String palabra2 = "Tecnologia";
+		palabrasClave.add(palabra1);
+		palabrasClave.add(palabra2);
+		
+		this.articulo = new ArticuloAcademico("IA Avanzada", autores, "Investigacion", "UNQ", palabrasClave);
+		
+		this.sistema = new SistemaDePublicaciones();
+		
+		CriterioAutor criterioAutor = new CriterioAutor("Carlos Rodriguez");
+		CriterioTitulo criterioTitulo = new CriterioTitulo("Programacion Funcional");
+		this.conector = new And(criterioAutor, criterioTitulo);
+		this.investigador = new Investigador(conector);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void funcionamientoDelObserverSinNotificar() {
+		sistema.suscribir(investigador);
+		sistema.publicar(articulo);
+		assertTrue(investigador.articulosDeInteres().isEmpty());
+	}
+	
+	@Test
+	public void funcionamientoDelObserverNotificando() {
+		List<Autor> autores = new ArrayList<Autor>();
+		Autor autor = new Autor("Carlos Rodriguez", "Universidad Nacional de Quilmes");
+		autores.add(autor);
+		
+		List<String> palabrasClave = new ArrayList<String>();
+		String palabra1 = "IA";
+		String palabra2 = "Tecnologia";
+		palabrasClave.add(palabra1);
+		palabrasClave.add(palabra2);
+		
+		ArticuloAcademico articulo2 = new ArticuloAcademico("Programacion Funcional", autores, "Investigacion", "UNQ", palabrasClave);
+		
+		sistema.suscribir(investigador);
+		sistema.publicar(articulo2);
+		assertFalse(investigador.articulosDeInteres().isEmpty());
 	}
 }
